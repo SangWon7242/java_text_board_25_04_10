a8a4131dcd96f322c6689010311e1d35b177ada0
SangWon7242 <gotkdqja@gmail.com>
2025-04-18 21:54:25 +0900

    ArticleService 도입

--- src/main/java/com/sbs/java/board/container/Container.java
+++ src/main/java/com/sbs/java/board/container/Container.java
@@ -1,17 +1,21 @@
 package com.sbs.java.board.container;
 
 import com.sbs.java.board.boundedContext.article.controller.ArticleController;
+import com.sbs.java.board.boundedContext.article.service.ArticleService;
 
 import java.util.Scanner;
 
 public class Container {
   public static Scanner sc;
 
+  public static ArticleService articleService;
   public static ArticleController articleController;
 
   // 프로그램 실행시 딱 한번 실행
   static {
     sc = new Scanner(System.in);
+
+    articleService = new ArticleService();
 
     articleController = new ArticleController();
   }
