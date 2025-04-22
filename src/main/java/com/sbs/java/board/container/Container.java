1e906f83026a0ab825abdaff3f79a303acb81f60
SangWon7242 <gotkdqja@gmail.com>
2025-04-22 19:23:23 +0900

    ArticleRepository 도입

--- src/main/java/com/sbs/java/board/container/Container.java
+++ src/main/java/com/sbs/java/board/container/Container.java
@@ -1,6 +1,7 @@
 package com.sbs.java.board.container;
 
 import com.sbs.java.board.boundedContext.article.controller.ArticleController;
+import com.sbs.java.board.boundedContext.article.repository.ArticleRepository;
 import com.sbs.java.board.boundedContext.article.service.ArticleService;
 
 import java.util.Scanner;
@@ -8,12 +9,15 @@ import java.util.Scanner;
 public class Container {
   public static Scanner sc;
 
+  public static ArticleRepository articleRepository;
   public static ArticleService articleService;
   public static ArticleController articleController;
 
   // 프로그램 실행시 딱 한번 실행
   static {
     sc = new Scanner(System.in);
+
+    articleRepository = new ArticleRepository();
 
     articleService = new ArticleService();
 
