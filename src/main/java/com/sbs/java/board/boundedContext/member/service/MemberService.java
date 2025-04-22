15eaf4c38e77b58360b667f9ce0097870a16999f
SangWon7242 <gotkdqja@gmail.com>
2025-04-22 21:07:02 +0900

    MemberService, MemberRepository 도입

---
+++ src/main/java/com/sbs/java/board/boundedContext/member/service/MemberService.java
@@ -0,0 +1,16 @@
+package com.sbs.java.board.boundedContext.member.service;
+
+import com.sbs.java.board.boundedContext.member.repository.MemberRepository;
+import com.sbs.java.board.container.Container;
+
+public class MemberService {
+  private MemberRepository memberRepository;
+
+  public MemberService() {
+    memberRepository = Container.memberRepository;
+  }
+
+  public void join(String username, String password, String name) {
+    memberRepository.join(username, password, name);
+  }
+}
